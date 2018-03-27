51fcb502b0da5dd6cafb7a38a0473ca95ff85f6f
hoslackochieng@gmail.com <hoslackochieng@gmail.com>
2018-03-27 23:17:34 +0300

    Designed cards for the services page and redesigned the logo

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,35 +1,29 @@
 import React, { Component } from 'react';
-import logo from '../logo.png';
+import logo from '../logo1.jpeg';
 import { Link } from 'react-router-dom';
 
 class Header extends Component {
 	render() {
 		return (
 			<div>
-				<header className="h-bg white tc pv4 avenir">
-					<a className="w3 h4" href="/" title="Home">
-						<img src={logo} className=" w3 h4 " alt="Mwachi Analytics" />
+				<header className="bg-white green tc pv4 avenir">
+					<a className="w8 h5" href="/" title="Home">
+						<img src={logo} className=" w8 h5" alt="Mwachi Analytics" />
 					</a>
-					<br />
-					<span className="top-5 normal f3 f2-ns mv0 lh-title">
-						Mwachi Analytics &amp; Consultancy
-					</span>
-
-					<h2 className="mt2 mb0 f6 fw4 ttu tracked">Data | Proof | Decisions</h2>
 					<nav className="bt bb tc mw7 center mt4">
-						<a className="f6 f5-l link bg-animate white dib pa3 ph4-l" href="/">
+						<a className="f6 f5-l link bg-animate green b dib pa3 ph4-l" href="/">
 							HOME
 						</a>
-						<Link to="/services" className="f6 f5-l link white dib pa3 ph4-l" href="/portfolio">
+						<Link to="/services" className="f6 f5-l link green b dib pa3 ph4-l" href="/portfolio">
 							OUR SERVICES
 						</Link>
-						<a className="f6 f5-l link white dib pa3 ph4-l" href="/">
+						<a className="f6 f5-l link green b dib pa3 ph4-l" href="/">
 							BLOG
 						</a>
-						<a className="f6 f5-l link white dib pa3 ph4-l" href="/">
+						<a className="f6 f5-l link green b dib pa3 ph4-l" href="/">
 							ABOUT US
 						</a>
-						<a className="f6 f5-l link white dib pa3 ph4-l" href="/">
+						<a className="f6 f5-l link green b dib pa3 ph4-l" href="/">
 							CONTACT US
 						</a>
 					</nav>
--- client/src/components/OurServices.js
+++ client/src/components/OurServices.js
@@ -3,11 +3,155 @@ import React, { Component } from 'react';
 class OurServices extends Component {
 	render() {
 		return (
-			<div className="h-bg black">
-				<h1>This is the services page</h1>
-				<h1>This is the services page</h1>
-				<h1>This is the services page</h1>
-				<h1>This is the services page</h1>
+			<div className="flex flex-column">
+				<div className="sv-bg pv4 flex flex-wrap">
+					<article class="bg-white mw5 ba b--black-10 mv4 outline w-24 pa3 mr2 center">
+						<div class="pv2 ph3">
+							<h1 class="f6 ttu tracked">Daily News Co.</h1>
+						</div>
+						<img
+							src="http://tachyons.io/img/cat-720.jpg"
+							class="w-100 db"
+							alt="Closeup photo of a tabby cat yawning."
+						/>
+						<div class="pa3">
+							<a href="#" class="link dim lh-title">
+								15 things every cat owner should know
+							</a>
+							<small class="gray db pv2">
+								AMP - <time>6 hours ago</time>
+							</small>
+						</div>
+					</article>
+					<article class="bg-white mw5 ba b--black-10 mv4 outline w-24 pa3 mr2 center">
+						<div class="pv2 ph3">
+							<h1 class="f6 ttu tracked">Daily News Co.</h1>
+						</div>
+						<img
+							src="http://tachyons.io/img/cat-720.jpg"
+							class="w-100 db"
+							alt="Closeup photo of a tabby cat yawning."
+						/>
+						<div class="pa3">
+							<a href="#" class="link dim lh-title">
+								15 things every cat owner should know
+							</a>
+							<small class="gray db pv2">
+								AMP - <time>6 hours ago</time>
+							</small>
+						</div>
+					</article>
+					<article class="bg-white mw5 ba b--black-10 mv4 outline w-24 pa3 mr2 center">
+						<div class="pv2 ph3">
+							<h1 class="f6 ttu tracked">Daily News Co.</h1>
+						</div>
+						<img
+							src="http://tachyons.io/img/cat-720.jpg"
+							class="w-100 db"
+							alt="Closeup photo of a tabby cat yawning."
+						/>
+						<div class="pa3">
+							<a href="#" class="link dim lh-title">
+								15 things every cat owner should know
+							</a>
+							<small class="gray db pv2">
+								AMP - <time>6 hours ago</time>
+							</small>
+						</div>
+					</article>
+					<article class="bg-white mw5 ba b--black-10 mv4 outline w-2 pa3 mr2 center">
+						<div class="pv2 ph3">
+							<h1 class="f6 ttu tracked">Daily News Co.</h1>
+						</div>
+						<img
+							src="http://tachyons.io/img/cat-720.jpg"
+							class="w-100 db"
+							alt="Closeup photo of a tabby cat yawning."
+						/>
+						<div class="pa3">
+							<a href="#" class="link dim lh-title">
+								15 things every cat owner should know
+							</a>
+							<small class="gray db pv2">
+								AMP - <time>6 hours ago</time>
+							</small>
+						</div>
+					</article>
+				</div>
+				<div className="sv-bg pv4 flex flex-wrap">
+					<article class="bg-white mw5 ba b--black-10 mv4 outline w-24 pa3 mr2 center">
+						<div class="pv2 ph3">
+							<h1 class="f6 ttu tracked">Daily News Co.</h1>
+						</div>
+						<img
+							src="http://tachyons.io/img/cat-720.jpg"
+							class="w-100 db"
+							alt="Closeup photo of a tabby cat yawning."
+						/>
+						<div class="pa3">
+							<a href="#" class="link dim lh-title">
+								15 things every cat owner should know
+							</a>
+							<small class="gray db pv2">
+								AMP - <time>6 hours ago</time>
+							</small>
+						</div>
+					</article>
+					<article class="bg-white mw5 ba b--black-10 mv4 outline w-24 pa3 mr2 center">
+						<div class="pv2 ph3">
+							<h1 class="f6 ttu tracked">Daily News Co.</h1>
+						</div>
+						<img
+							src="http://tachyons.io/img/cat-720.jpg"
+							class="w-100 db"
+							alt="Closeup photo of a tabby cat yawning."
+						/>
+						<div class="pa3">
+							<a href="#" class="link dim lh-title">
+								15 things every cat owner should know
+							</a>
+							<small class="gray db pv2">
+								AMP - <time>6 hours ago</time>
+							</small>
+						</div>
+					</article>
+					<article class="bg-white mw5 ba b--black-10 mv4 outline w-24 pa3 mr2 center">
+						<div class="pv2 ph3">
+							<h1 class="f6 ttu tracked">Daily News Co.</h1>
+						</div>
+						<img
+							src="http://tachyons.io/img/cat-720.jpg"
+							class="w-100 db"
+							alt="Closeup photo of a tabby cat yawning."
+						/>
+						<div class="pa3">
+							<a href="#" class="link dim lh-title">
+								15 things every cat owner should know
+							</a>
+							<small class="gray db pv2">
+								AMP - <time>6 hours ago</time>
+							</small>
+						</div>
+					</article>
+					<article class="bg-white mw5 ba b--black-10 mv4 outline w-2 pa3 mr2 center">
+						<div class="pv2 ph3">
+							<h1 class="f6 ttu tracked">Daily News Co.</h1>
+						</div>
+						<img
+							src="http://tachyons.io/img/cat-720.jpg"
+							class="w-100 db"
+							alt="Closeup photo of a tabby cat yawning."
+						/>
+						<div class="pa3">
+							<a href="#" class="link dim lh-title">
+								15 things every cat owner should know
+							</a>
+							<small class="gray db pv2">
+								AMP - <time>6 hours ago</time>
+							</small>
+						</div>
+					</article>
+				</div>
 			</div>
 		);
 	}

c73fdeeec537e2405640081ef0e1ce8a67330bfa
hoslackochieng@gmail.com <hoslackochieng@gmail.com>
2018-06-09 23:55:21 +0300

    [fix] Fix failing deployment

--- index.js
+++ index.js
@@ -8,11 +8,13 @@ const path = require('path');
 const authRoutes = require('./routes/authRoutes');
 const orderRoutes = require('./routes/orderRoutes');
 
+
 const app = express();
 
 const port = process.env.PORT || 5000;
 const host = process.env.HOST || 'localhost';
 const db_url = process.env.MONGO_URL || 'mongodb://hos:amondi99@localhost:27017/mwachi';
+
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
@@ -37,6 +39,7 @@ mongoose.connection.on('connected', () => {
   });
 });
 
+
 if (process.env.NODE_ENV === 'production') {
   // Express will serve up production assets
   // like our main.js file, or main.css file!
@@ -47,7 +50,7 @@ if (process.env.NODE_ENV === 'production') {
   // Express will serve up the index.html file
   // if it doesn't recognize the route
   app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '..', 'client', 'build', 'index.html'));
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
   });
 } else {
   app.use('/auth', authRoutes);
